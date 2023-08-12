ab2966096b78db0859575e7064b01621ff1f9949
Diego León Acevedo Arenas <diego.acevedo@proveedores.lineadirecta.com.co>
2023-08-11 23:50:11 -0500

    Se agrega proyecto

---
+++ src/helpers/constants.js
@@ -0,0 +1,20 @@
+export const PATH = {
+  INIT: "*",
+  ALL: "/*",
+  SEARCH: "/",
+  RESULT: "/items",
+  //   RESULT: "/items?search=",
+  DETAIL: "/items/:id",
+};
+
+export const API = {
+  SEARCH: "sites/MLA/search?q=:query",
+  RESULT: "/items/:id",
+  DETAIL: "/items/:id​/description",
+};
+
+export const VIEWS = {
+  SEARCH: "search",
+  RESULT: "result",
+  DETAIL: "detail",
+};
---
+++ src/routers/DashBoardRouter.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Search } from "../components/views/search";
+import { Result } from "../components/views/Result";
+import { Detail } from "../components/views/Detail";
+import { PATH } from "../helpers/constants";
+
+export const DashBoardRouter = () => {
+  return (
+    <Routes>
+      <Route path={PATH.SEARCH} element={<Search />} />
+      <Route path={PATH.RESULT} element={<Result />} />
+      <Route path={PATH.DETAIL} element={<Detail />} />
+      <Route path={PATH.ALL} element={<Navigate to={PATH.SEARCH} />} />
+    </Routes>
+  );
+};
