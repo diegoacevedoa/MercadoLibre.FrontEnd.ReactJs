2a41912c2ae7223935f947bfd9f0a5d7392a5d0d
Diego León Acevedo Arenas <diego.acevedo@proveedores.lineadirecta.com.co>
2023-08-15 16:59:24 -0500

    Se realiza consulta desde ventana Result y captura de parametros de url

--- src/components/views/result/Result.jsx
+++ src/components/views/result/Result.jsx
@@ -1,50 +1,69 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useMemo } from "react";
 import { Container, Col, Row } from "react-bootstrap";
 import { CardItem } from "./CardItem";
 import { ProductContext } from "../../../context/ProductContext";
 import "./Result.scss";
+import { Loading } from "../../ui/loading";
+import { useSearchParams } from "react-router-dom";
 
 export const Result = () => {
-  const { allData } = useContext(ProductContext);
+  const [queryParameters] = useSearchParams();
+  const { loading, allData, handleGetAllProducts } = useContext(ProductContext);
+  const searchParameter = queryParameters.get("search");
+
+  useEffect(() => {
+    if (searchParameter != undefined && searchParameter != "") {
+      handleGetAllProducts(searchParameter);
+    }
+  }, [searchParameter]);
+
+  const getResults = useMemo(() => {
+    return (
+      <Container className="mb-body-result" fluid>
+        <Row>
+          <Col
+            lg={{ span: 8, offset: 2 }}
+            md={{ span: 8, offset: 2 }}
+            sm={{ span: 10, offset: 1 }}
+            xs={{ span: 10, offset: 1 }}
+          >
+            <span className="breadcrumb">
+              {allData?.categories.toString().replaceAll(",", " > ")}
+            </span>
+          </Col>
+          <Col
+            lg={{ span: 2, offset: 0 }}
+            md={{ span: 2, offset: 0 }}
+            sm={{ span: 1, offset: 0 }}
+            xs={{ span: 1, offset: 0 }}
+          ></Col>
+        </Row>
+        <Row>
+          <Col
+            lg={{ span: 8, offset: 2 }}
+            md={{ span: 8, offset: 2 }}
+            sm={{ span: 10, offset: 1 }}
+            xs={{ span: 10, offset: 1 }}
+          >
+            {allData?.items?.map((item, idx) => (
+              <CardItem key={item.id} data={item} />
+            ))}
+          </Col>
+          <Col
+            lg={{ span: 2, offset: 0 }}
+            md={{ span: 2, offset: 0 }}
+            sm={{ span: 1, offset: 0 }}
+            xs={{ span: 1, offset: 0 }}
+          ></Col>
+        </Row>
+      </Container>
+    );
+  }, [allData]);
 
   return (
-    <Container className="mb-body-result" fluid>
-      <Row>
-        <Col
-          lg={{ span: 8, offset: 2 }}
-          md={{ span: 8, offset: 2 }}
-          sm={{ span: 10, offset: 1 }}
-          xs={{ span: 10, offset: 1 }}
-        >
-          <span className="breadcrumb">
-            {allData?.categories.toString().replaceAll(",", " > ")}
-          </span>
-        </Col>
-        <Col
-          lg={{ span: 2, offset: 0 }}
-          md={{ span: 2, offset: 0 }}
-          sm={{ span: 1, offset: 0 }}
-          xs={{ span: 1, offset: 0 }}
-        ></Col>
-      </Row>
-      <Row>
-        <Col
-          lg={{ span: 8, offset: 2 }}
-          md={{ span: 8, offset: 2 }}
-          sm={{ span: 10, offset: 1 }}
-          xs={{ span: 10, offset: 1 }}
-        >
-          {allData?.items?.map((item, idx) => (
-            <CardItem key={item.id} data={item} />
-          ))}
-        </Col>
-        <Col
-          lg={{ span: 2, offset: 0 }}
-          md={{ span: 2, offset: 0 }}
-          sm={{ span: 1, offset: 0 }}
-          xs={{ span: 1, offset: 0 }}
-        ></Col>
-      </Row>
-    </Container>
+    <>
+      {getResults}
+      <Loading show={loading} />
+    </>
   );
 };
